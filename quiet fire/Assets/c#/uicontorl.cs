4d2e8749cec090b006f9c54f80b5f2cca619c16b
kevin0106 <kevin910106@gmail.com>
2022-11-24 00:11:31 +0800

    11/23(1)

---
+++ quiet fire/Assets/c#/uicontorl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class uicontorl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+    public void start()
+    {
+        SceneManager.LoadScene("how to play");
+    }
+    public void credit()
+    {
+        SceneManager.LoadScene("credit");
+    }
+    public void play()
+    {
+        SceneManager.LoadScene("1f");
+    }
+   
+}
